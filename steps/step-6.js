7b5b2b1a564b31c873fd981d056f285f57eb02fe
Tom Peres <tomp@wix.com>
2016-11-24 17:23:08 +0200

    step-6 - authorization (storage and database)

---
+++ steps/step-6.js
@@ -0,0 +1,9 @@
+/**
+ * STEP #6 - Authorization - database and storage
+ * ========
+ * NOTES:
+ * You don't need to implement any js code here.
+ * You should create two authorization files:
+ * 1) database-rules.json (https://firebase.google.com/docs/database/security/) > Update the rules to the following which only allows authenticated users to read and write
+ * 2) storage.rules (https://firebase.google.com/docs/storage/security/start) > Update the rules to the following which only allows users to write to their own folder
+ **/
