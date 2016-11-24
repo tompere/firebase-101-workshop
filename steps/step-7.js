b749d159c611d0dff91097c34e43a21b005ec0dc
Tom Peres <tomp@wix.com>
2016-11-24 17:36:23 +0200

    step-7 - hosting and deploy

---
+++ steps/step-7.js
@@ -0,0 +1,11 @@
+/**
+ * STEP #6 - Hosting - deploy your webapp to production (CDN)
+ * ========
+ * NOTES:
+ * You don't need to implement any js code here.
+ * 1) Install node.js (https://nodejs.org/en/).
+ * 2) In cmd/terminal enter `npm install -g firebase-tools`.
+ * 3) In cmd/terminal enter `firebase login` (and manually login to your google account in opened browser).
+ * 4) In cmd/terminal enter `firebase deploy`.
+ * 5) check your public url (see 'Hosting Site' in previous command output).
+ * **/
